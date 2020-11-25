d2894c7c89ffb261348f439b822dd11acd7f4ca7
wangmanong <1293702896@qq.com>
2020-11-25 16:22:09 +0800

    04_常用内置组件讲解

---
+++ pages/image/image.js
@@ -0,0 +1,80 @@
+// pages/image/image.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: { 
+    imgPath:[]
+  },
+  handleChoose(){
+    //系统API，让用户在相册中选择图片（或者拍照）
+    const _this = this
+    wx.chooseImage({
+      success:function(res){
+        const path = res.tempFiles[0]
+        _this.setData({
+          imgPath : path
+        })
+      }
+    })
+  },
+  handleImgLoad(){
+    console.log('图片加载完成')
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
