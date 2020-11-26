f16396772a5107ada9e24d5f7aedd142875f8fae
wangmanong <1293702896@qq.com>
2020-11-26 23:22:49 +0800

    11_展示弹窗及分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,60 +7,59 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  showToast(){
+    wx.showToast({
+      title: '你好啊',
+      duration:3000,
+      success:function(){
+        console.log('弹出成功')
+      },
+      complete:function(){
+        console.log('成败都执行')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  showModal(){
+    wx.showModal({
+      title: '我是标题',
+      content:'我是内容',
+      success:function(res){
+        if(res.confirm){
+          console.log('点击确定')
+        }
+        if(res.cancel){
+          console.log('点击取消')
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  showLoading(){
+    wx.showLoading({
+      title: '加载中',
+      mask:true
+    })
+    setTimeout(() => {
+      wx.hideLoading({
+        success: (res) => {
+          console.log('加载完成')
+        },
+      })
+    }, 1000);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  showAction(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor:'red',
+      success:function(res){
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage:function(options){
+    return {
+      title:'你好啊',
+      page:'/pages/about/about',
+      imageUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1606411742593&di=a87b97a3f30c96e98107af35d9d422f1&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_water_img%2F19%2F03%2F11%2Fab8cada0f0d17873deb389d8537115dc.jpg'
+    }
   }
 })
